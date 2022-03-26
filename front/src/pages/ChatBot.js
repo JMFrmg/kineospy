408cd6116fe06a95bd98223967179da7a52bb2e9
klein <allan.klein@epitech.eu>
2022-03-26 15:15:27 +0100

    add front

---
+++ front/src/pages/ChatBot.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Row, Col } from 'antd';
+import Chat, { Bubble, useMessages } from '@chatui/core';
+import '@chatui/core/dist/index.css';
+
+const ChatBot = () => {
+  const { messages, appendMsg, setTyping } = useMessages([]);
+
+  function handleSend(type, val) {
+    if (type === 'text' && val.trim()) {
+      appendMsg({
+        type: 'text',
+        content: { text: val },
+        position: 'right',
+      });
+
+      setTyping(true);
+
+      setTimeout(() => {
+        appendMsg({
+          type: 'text',
+          content: { text: 'Bala bala' },
+        });
+      }, 1000);
+    }
+  }
+
+  function renderMessageContent(msg) {
+    const { content } = msg;
+    return <Bubble content={content.text} />;
+  }
+
+  return (
+    <Chat
+      navbar={{ title: 'Assistant' }}
+      messages={messages}
+      renderMessageContent={renderMessageContent}
+      onSend={handleSend}
+    />
+  );
+};
+
+export default ChatBot;
