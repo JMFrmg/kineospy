5e6a06d0f38767eea5bd0ddcfd631443d7e37578
apo <apo@2crsi.com>
2022-03-26 14:15:29 +0100

    boilerplate django v1.1

---
+++ hh_django_react/back_end/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+# def index(request):
+#     context = {}
+#     context['segment'] = 'index'
+#
+#     html_template = loader.get_template('index.html')
+#     return HttpResponse(html_template.render(context, request))
